8efee37d72fa6643dd92cad54b910e3a68945ae2
Muhammad Affan <145008238+MuhammadAffandi24@users.noreply.github.com>
2026-03-10 10:28:12 +0700

    Create Query_Penyeragaman.sql
    Abis Ingest Jalanin ini

---
+++ Query_Penyeragaman.sql
@@ -0,0 +1,27 @@
+UPDATE putusan_pajak
+SET amar_putusan = CASE 
+    -- Kelompok Menolak
+    WHEN amar_putusan ILIKE '%tolak%' THEN 'Menolak'
+    
+    -- Kelompok Mengabulkan Seluruhnya
+    WHEN amar_putusan ILIKE '%seluruh%' 
+         OR amar_putusan IN ('kabul', 'Kabul', 'Mengabulkan', 'mengabulkan') 
+         OR amar_putusan ILIKE '%menerima dan mengabulkan%seluruhnya%' THEN 'Mengabulkan Seluruhnya'
+    
+    -- Kelompok Mengabulkan Sebagian
+    WHEN amar_putusan ILIKE '%sebagian%' THEN 'Mengabulkan Sebagian'
+    
+    -- Kelompok Membatalkan
+    WHEN amar_putusan ILIKE '%batal%' THEN 'Membatalkan'
+    
+    -- Kelompok Tidak Dapat Diterima
+    WHEN amar_putusan ILIKE '%tidak dapat diterima%' THEN 'Tidak Dapat Diterima'
+    
+    -- Kelompok Lain-lain (Termasuk Null dan redundansi lainnya)
+    WHEN amar_putusan IS NULL 
+         OR amar_putusan IN ('null', 'lainnya', 'Lain-lain', 'Lainnya') 
+         OR amar_putusan ILIKE '%menguatkan%' 
+         OR amar_putusan ILIKE '%menambah pajak%' THEN 'Lain-lain'
+    
+    ELSE 'Lain-lain'
+END;
